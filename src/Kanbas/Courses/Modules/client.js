c32e8183865a40b95c4830b017a261380db03988
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-11-13 21:05:10 -0800

    commit

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -4,13 +4,12 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 const MODULES_URL = `${API_BASE}/modules`;
 
 export const updateModule = async (module) => {
-  const response = await axios.
-    put(`${MODULES_URL}/${module._id}`, module);
+  const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
   return response.data;
 };
+
 export const deleteModule = async (moduleId) => {
-  const response = await axios
-    .delete(`${MODULES_URL}/${moduleId}`);
+  const response = await axios.delete(`${MODULES_URL}/${moduleId}`);
   return response.data;
 };
 
@@ -21,8 +20,8 @@ export const createModule = async (courseId, module) => {
   );
   return response.data;
 };
+
 export const findModulesForCourse = async (courseId) => {
-  const response = await axios
-    .get(`${COURSES_URL}/${courseId}/modules`);
+  const response = await axios.get(`${COURSES_URL}/${courseId}/modules`);
   return response.data;
 };
