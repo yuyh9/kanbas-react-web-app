aa3c47384cd45ebbb40018f61d649261499f641d
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-12-01 22:32:53 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
-import Project from "./project";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="/Labs" />} />
-          <Route path="/project/*" element={<Project />} />
           <Route path="/hello" element={<HelloWorld />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
